0ad15f370f68ad55aa57e5bf56460a7c8b757bc4
Benjamin Bulis <bbulis@student.tgm.ac.at>
2021-03-11 14:15:19 +0100

    Added Database Model for SQLite database

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=255)
+    answer_count = models.IntegerField(default=0)
+
+class Answer(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=255)
