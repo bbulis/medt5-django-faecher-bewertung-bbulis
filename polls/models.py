8ce2a3e0ee12f968cac83a4770ffc82ec6f5903f
Benjamin Bulis <bbulis@student.tgm.ac.at>
2021-03-11 14:27:48 +0100

    Added toString functions to classes

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,14 @@ class Subject(models.Model):
     subject_name = models.CharField(max_length=255)
     answer_count = models.IntegerField(default=0)
 
+    def __str__(self) -> str:
+        ret = {'subject_name': self.subject_name, 'answer_count': self.answer_count}
+        return ret
+
 class Answer(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        ret = {'subject_name': self.subject, 'answer_text': self.answer_text}
+        return ret
