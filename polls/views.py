b05ef78ce495723833a65723c62bdc66bae268ed
Benjamin Bulis <bbulis@student.tgm.ac.at>
2021-03-12 11:59:23 +0100

    Added html for rendering and displaying subjects

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Subject
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    subject_list = Subject.objects.all()
+    context = {'subject_list': subject_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, subject_id):
     return HttpResponse('Subject ID %s' % subject_id)
